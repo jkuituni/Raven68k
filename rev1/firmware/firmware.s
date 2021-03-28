ef36b9d35f8a374bd044e68460c22c4362e67109
Jani Kuituniemi <>
2021-03-28 22:05:20 +0300

    Formatting updates and bugfixes.

--- rev1/firmware/firmware.s
+++ rev1/firmware/firmware.s
@@ -47,6 +47,8 @@
 .long   .getChar                | Read a char into d0
 .long   .prntMsg                | Print a message in a0
 .long   .initDuart              | Init the 68681 Duart to 9600,8,n,1
+.long   .SRecUpload             | Upload Motorola SRecord format binary
+.long   .SRecExec               | Execute Motorola SRecord format biary 
 
 // ---- Firmware Init ----
 .init:
@@ -126,147 +128,147 @@ _crxrdy:
 
 // -- SRecord utility routines
 .SRecUpload:
-  jsr     .getChar              | get a character from host
-  cmp.b   #'S', %d0             | records must start with 'S'
-  bne     .SRecUpload           | if not, repeat getting chars until we get an 'S'
-  jsr     .getChar              | get character after 'S'
-  cmp.b   #'9', %d0             | test for an S9 terminator
+  jsr     .getChar              | Get a character from host
+  cmp.b   #'S', %d0             | Records must start with 'S'
+  bne     .SRecUpload           | If not, repeat getting chars until we get an 'S'
+  jsr     .getChar              | Get character after 'S'
+  cmp.b   #'9', %d0             | Test for an S9 terminator
   beq     _ld_s9                | Handle it..
-  cmp.b   #'8', %d0             | test for an S8 terminator
+  cmp.b   #'8', %d0             | Test for an S8 terminator
   beq     _ld_s8                | Handle it..
-  cmp.b   #'1', %d0             | test for an S1 record
+  cmp.b   #'1', %d0             | Test for an S1 record
   beq     _ld_s1                | Handle it..
-  cmp.b   #'2', %d0             | test for an S2 record
+  cmp.b   #'2', %d0             | Test for an S2 record
   beq     _ld_s2                | Handle it..
   bra     .SRecUpload           | Unknown terminator -> Go back to beginning
 _ld_s1:
-  clr.b   %d3                   | clear the checksum
-  jsr     _ld_get_byte          | read the S1 byte count and address
-  sub.b   #3, %d0               | subtract 3 from record length
-  move.b  %d0, %d2              | save byte count in d2
-  clr.l   %d0                   | clear address accumulator
-  jsr     _ld_get_byte          | get MS byte of load address
-  asl.l   #8, %d0               | move it to MS position
-  jsr     _ld_get_byte          | get LS byte in D2
+  clr.b   %d3                   | Clear the checksum
+  jsr     _ld_get_byte          | Read the S1 byte count and address
+  sub.b   #3, %d0               | Subtract 3 from record length
+  move.b  %d0, %d2              | Save byte count in d2
+  clr.l   %d0                   | Clear address accumulator
+  jsr     _ld_get_byte          | Get MS byte of load address
+  asl.l   #8, %d0               | Move it to MS position
+  jsr     _ld_get_byte          | Get LS byte in D2
   move.l  %d0, %a2              | a2 points to destination of data
-  bra     _ld_data              | load the data
+  bra     _ld_data              | Load the data
 _ld_s2:
-  clr.b   %d3                   | clear the checksum
-  jsr     _ld_get_byte          | read the S2 byte count and address
-  sub.b   #4, %d0               | calculate size of data field
+  clr.b   %d3                   | Clear the checksum
+  jsr     _ld_get_byte          | Read the S2 byte count and address
+  sub.b   #4, %d0               | Calculate size of data field
   move.b  %d0, %d2              | d2 contains data bytes to read
-  clr.l   %d0                   | clear address accumulator
-  jsr     _ld_get_byte          | read most sig byte of address
-  asl.l   #8, %d0               | move it one byte left
-  jsr     _ld_get_byte          | read middle byte of address
-  asl.l   #8, %d0               | move it one byte left
-  jsr     _ld_get_byte          | read least sig byte of address
+  clr.l   %d0                   | Clear address accumulator
+  jsr     _ld_get_byte          | Read most sig byte of address
+  asl.l   #8, %d0               | Move it one byte left
+  jsr     _ld_get_byte          | Read middle byte of address
+  asl.l   #8, %d0               | Move it one byte left
+  jsr     _ld_get_byte          | Read least sig byte of address
   move.l  %d0, %a2              | a2 points to destination of record
-  bra     _ld_data              | load the data
+  bra     _ld_data              | Load the data
 _ld_s8:
-  clr.b   %d3                   | clear the checksum
-  jsr     _ld_get_byte          | read the S8 byte count and address
+  clr.b   %d3                   | Clear the checksum
+  jsr     _ld_get_byte          | Read the S8 byte count and address
   move.b  %d0, %d2              | d2 contains data bytes to read
-  clr.l   %d0                   | clear address accumulator
-  jsr     _ld_get_byte          | read most sig byte of address
-  asl.l   #8, %d0               | move it one byte left
-  jsr     _ld_get_byte          | read middle byte of address
-  asl.l   #8, %d0               | move it one byte left
-  jsr     _ld_get_byte          | read least sig byte of address
+  clr.l   %d0                   | Clear address accumulator
+  jsr     _ld_get_byte          | Read most sig byte of address
+  asl.l   #8, %d0               | Move it one byte left
+  jsr     _ld_get_byte          | Read middle byte of address
+  asl.l   #8, %d0               | Move it one byte left
+  jsr     _ld_get_byte          | Read least sig byte of address
   move.l  %d0, %a2              | a2 points to destination of record
-  bra     _ld_terminate         | return
+  bra     _ld_terminate         | Return
 _ld_s9:
-  clr.b   %d3                   | clear the checksum
-  jsr     _ld_get_byte          | read the S9 byte count and address
-  move.b  %d0, %d2              | save byte count in d2
-  clr.l   %d0                   | clear address accumulator
-  jsr     _ld_get_byte          | get MS byte of load address
-  asl.l   #8, %d0               | move it to MS position
-  jsr     _ld_get_byte          | get LS byte in D2
+  clr.b   %d3                   | Clear the checksum
+  jsr     _ld_get_byte          | Read the S9 byte count and address
+  move.b  %d0, %d2              | Save byte count in d2
+  clr.l   %d0                   | Clear address accumulator
+  jsr     _ld_get_byte          | Get MS byte of load address
+  asl.l   #8, %d0               | Move it to MS position
+  jsr     _ld_get_byte          | Get LS byte in D2
   move.l  %d0, %a2              | a2 points to destination of data
-  bra     _ld_terminate         | return
+  bra     _ld_terminate         | Return
 _ld_data:
-  jsr     _ld_get_byte          | get byte of data for loading
-  move.b  %d0, %a2@+            | store it
-  sub.b   #1, %d2               | decrement byte counter
-  bne     _ld_data              | repeat until count = 0
-  jsr     _ld_get_byte          | read checksum
-  add.b   #1, %d3               | add 1 to total checksum
-  beq     _ld_data_ok           | if zero then  draw a dot to console
-  or.b    #0b00001000, %d7      | else set checksum error bit,
+  jsr     _ld_get_byte          | Get byte of data for loading
+  move.b  %d0, %a2@+            | Store it
+  sub.b   #1, %d2               | Decrement byte counter
+  bne     _ld_data              | Repeat until count = 0
+  jsr     _ld_get_byte          | Read checksum
+  add.b   #1, %d3               | Add 1 to total checksum
+  beq     _ld_data_ok           | If zero then  draw a dot to console
+  or.b    #0b00001000, %d7      | Else set checksum error bit,
 _ld_terminate:
-  btst.b  #0, %d7               | test for input errors
-  beq     _ld_chksum            | if no errors check the checksum
-  lea     _msgErrNotHex, %a0    | send error message to console
+  btst.b  #0, %d7               | Test for input errors
+  beq     _ld_chksum            | If no errors check the checksum
+  lea     _msgErrNotHex, %a0    | Send error message to console
   jsr     .prntMsg
 _ld_chksum:
-  btst.b  #3, %d7               | test for checksum errors
-  beq     _ld_exit              | if no errors return
-  lea     _msgErrChksum, %a0    | send error message to console
+  btst.b  #3, %d7               | Test for checksum errors
+  beq     _ld_exit              | If no errors return
+  lea     _msgErrChksum, %a0    | Send error message to console
   jsr     .prntMsg
   bra     _ld_exit
 _ld_data_ok:
-  move.b  #'.', %d0             | send a '.' to console
+  move.b  #'.', %d0             | Send a '.' to console
   jsr     .prntChar
-  bra     .SRecUpload           | get next record
+  bra     .SRecUpload           | Get next record
 _ld_exit:
-  cmpi.l  #0, %d7               | check for errors
-  bne     .SRecError            | errors found, offer to reset
-  lea     _msgSRecLoaded, %a0   | send srec loaded message
+  cmpi.l  #0, %d7               | Check for errors
+  bne     .SRecError            | Errors found, offer to reset
+  lea     _msgSRecLoaded, %a0   | Send SRec loaded message
   jsr     .prntMsg
-  move.l  %a2, %d0              | get start address from srec
-  jsr     .send_hex_l           | output the starting address
+  move.l  %a2, %d0              | Get start address from SRec
+  jsr     .send_hex_l           | Output the starting address
 _ld_get_byte:
   jsr     .get_hex_b
-  add.b   %d0, %d3              | update checksum
+  add.b   %d0, %d3              | Update checksum
   jmp     .run                  | SRec loaded -> Back to main routine
 .send_hex_n:
-  movem.l %d0, %a7@-            | save d0 to stack
-  and.b   #0x0f, %d0            | mask off MS nybble
-  add.b   #0x30, %d0            | convert to ascii
-  cmp.b   #0x39, %d0            | ascii = hex + 0x30
-  bls     _send_low             | if ascii <= 39 then print and exit
-  add.b   #0x07, %d0            | else ascii = hex + 7
+  movem.l %d0, %a7@-            | Save d0 to stack
+  and.b   #0x0f, %d0            | Mask off MS nybble
+  add.b   #0x30, %d0            | Convert to ascii
+  cmp.b   #0x39, %d0            | ASCII = HEX + 0x30
+  bls     _send_low             | If ASCII <= 39 then print and exit
+  add.b   #0x07, %d0            | Else ASCII = HEX + 7
 _send_low:
-  jsr     .prntChar             | print the character to the console
-  movem.l %a7@+, %d0            | restore d0 from stack
+  jsr     .prntChar             | Print the character to the console
+  movem.l %a7@+, %d0            | Restore d0 from stack
   rts
 .send_hex_b:
-  ror.b   #4, %d0               | move MS nybble to the LS nybble
-  jsr     .send_hex_n           | send the MS nybble hex
-  rol.b   #4, %d0               | restore the LS nybble
-  bra     .send_hex_n           | send the LS nybble hex and return
+  ror.b   #4, %d0               | Move MS nybble to the LS nybble
+  jsr     .send_hex_n           | Send the MS nybble hex
+  rol.b   #4, %d0               | Restore the LS nybble
+  bra     .send_hex_n           | Send the LS nybble hex and return
 .send_hex_w:
-  ror.w   #8, %d0               | move MS byte to the LS byte
-  jsr     .send_hex_b           | send the MS byte hex
-  rol.w   #8, %d0               | restore the LS byte
-  bra     .send_hex_b           | send the LS byte hex and return
+  ror.w   #8, %d0               | Move MS byte to the LS byte
+  jsr     .send_hex_b           | Send the MS byte hex
+  rol.w   #8, %d0               | Restore the LS byte
+  bra     .send_hex_b           | Send the LS byte hex and return
 .send_hex_l:
-  swap    %d0                   | move MS word to the LS word
-  jsr     .send_hex_w           | send the MS word hex
-  swap    %d0                   | restore the LS word
-  bra     .send_hex_w           | send the LS word hex and return
+  swap    %d0                   | Move MS word to the LS word
+  jsr     .send_hex_w           | Send the MS word hex
+  swap    %d0                   | Restore the LS word
+  bra     .send_hex_w           | Send the LS word hex and return
 .get_hex_n:
-  jsr     .getChar              | get character from console
-  sub.b   #0x30, %d0            | drop ascii down to numbers
-  bmi     _not_hex              | check we didn't underflow on not numerics
-  cmp.b   #0x09, %d0            | check we're a number <= 9
-  ble     _hex_ok               | ok, we got a number, job done
-  sub.b   #0x07, %d0            | move ascii down to 'A'
-  cmp.b   #0x0F, %d0            | check we're between A-F
-  ble     _hex_ok               | ok, we are in A-F range, job done
+  jsr     .getChar              | Get character from console
+  sub.b   #0x30, %d0            | Drop ASCII down to numbers
+  bmi     _not_hex              | Check we didn't underflow on not numerics
+  cmp.b   #0x09, %d0            | Check we're a number <= 9
+  ble     _hex_ok               | OK, we got a number, job done
+  sub.b   #0x07, %d0            | Move ASCII down to 'A'
+  cmp.b   #0x0F, %d0            | Check we're between A-F
+  ble     _hex_ok               | OK, we are in A-F range, job done
 _not_hex:
-  or.b    #1, %d7               | not a hex value, set flag
+  or.b    #1, %d7               | Not a HEX value, set flag
 _hex_ok:
   rts
 .get_hex_b:
-  movem.l %d1, %a7@-            | save d1 to stack
-  jsr     .get_hex_n            | get first hex character
-  asl.b   #4, %d0               | move value to MS nybble position
-  move.b  %d0, %d1              | save MS nybble in d1
-  bsr     .get_hex_n            | get seccond hex character
-  add.b   %d1, %d0              | merge the MS and LS nybbles
-  movem.l %a7@+, %d1            | restore d1 from stack
+  movem.l %d1, %a7@-            | Save d1 to stack
+  jsr     .get_hex_n            | Get first hex character
+  asl.b   #4, %d0               | Move value to MS nybble position
+  move.b  %d0, %d1              | Save MS nybble in d1
+  bsr     .get_hex_n            | Get second HEX character
+  add.b   %d1, %d0              | Merge the MS and LS nybbles
+  movem.l %a7@+, %d1            | Restore d1 from stack
   rts
 .SRecError:
   lea     _msgSRecErr, %a0      | Set SRec Error message pointer
@@ -313,7 +315,7 @@ _msgPrompt:     .asciz  "Ready:\r\n"
 _msgHelp:       .ascii  "Commands:\r\n"
                 .ascii  "(U)pload SRec (S19/S28) file\r\n"
                 .ascii  "(E)xecute SRec file\r\n"
-                .ascii  "(H)elp\r\n""
+                .ascii  "(H)elp\r\n"
                 .asciz  "(R)eset\r\n\r\n"
 .align(2)
 _msgUnknown:    .asciz  "Unknown command!\r\n"
